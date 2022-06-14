e373046fdfa232a26281b067e2368c12c5790722
ricoatsproutasia <arisandy.rico@sproutasia.com>
2022-06-14 10:02:05 +0700

    first init

---
+++ app/Models/Flight.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+    use HasFactory;
+
+    protected $table = 'my_flights';
+    protected $primaryKey = "flight_id";
+    // attributes
+    protected $attributes = [
+        'delayed' => false,
+    ];
+
+}
